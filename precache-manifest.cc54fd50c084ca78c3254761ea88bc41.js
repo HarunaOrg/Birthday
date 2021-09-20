bd9e17a109b0499ad28bc9d2ecd7c562a54ac09c
Haruna Popoola <haruna.popoola@yahoo.com>
2021-09-20 09:12:12 +0100

    Updates

---
+++ precache-manifest.cc54fd50c084ca78c3254761ea88bc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8457868b51d42795272ce85464ef35",
+    "url": "/Birthday/index.html"
+  },
+  {
+    "revision": "de8f18d757cb6ae16b4b",
+    "url": "/Birthday/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "2f3fcaf740248c0c797e",
+    "url": "/Birthday/static/js/2.dc5335ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday/static/js/2.dc5335ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8f18d757cb6ae16b4b",
+    "url": "/Birthday/static/js/main.dcb1dba3.chunk.js"
+  },
+  {
+    "revision": "3fa7c4fe94ca6c0b4250",
+    "url": "/Birthday/static/js/runtime-main.fbeea47d.js"
+  }
+]);
